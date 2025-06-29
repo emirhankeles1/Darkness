334b237f94e4b156c9ff8843aa790d7a07d39d21
emirhankeles1 <80382967+emirhankeles1@users.noreply.github.com>
2025-06-29 10:19:46 +0300

    Create rivals.lua

---
+++ rivals.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/emirhankeles1/Darkness/refs/heads/main/esp_rivals.lua"))()
+wait(1) -- darkhub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/emirhankeles1/Darkness/refs/heads/main/aim_rivals.lua"))()
